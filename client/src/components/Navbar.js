c7c1c0e048620ec8b79ab7b7035a60cf1e42139e
Emma HB <emmanuelle.bolognini@gmail.com>
2022-02-12 16:13:13 +0100

    Auth Components - logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { logout } from './auth/auth-service';
+import { Link } from 'react-router-dom'; 
 
 class Navbar extends Component {
+
+  handleLogout = () => {
+    logout()
+      .then(() => {
+        this.props.updateUser(false);
+      })
+      .catch(err => console.log('error', err))
+  }
+
   render() {
     return (
       <div className="navbar">
-        <img alt="Sprintly."></img>
-        <img className="logout-icon" src={'/assets/Logout.png'} alt="logout icon"/>
+        <Link to={"/"}><img alt="Sprintly."></img></Link>
+        <button className='logout-btn' onClick={this.handleLogout}><img className="logout-icon" src={'/assets/Logout.png'} alt="logout icon"/></button>
       </div>
     )
   }
